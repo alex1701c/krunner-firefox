4eae3fcb878cd61f69ea5461adbd1ba2f6b00c52
alex1701c <alex1701c.dev@gmx.netyt>
2020-02-23 19:47:56 +0100

    Finish refactoring of datatype, improve actions config

--- src/config/firefoxprofilerunner_config.h
+++ src/config/firefoxprofilerunner_config.h
@@ -7,31 +7,35 @@
 #include "profile/ProfileManager.h"
 #include "kcmutils_version.h"
 
-enum ProfileWidgetType{
-    Normal, Private, ProxychainsNormal, ProxychainsPrivate
+enum ProfileWidgetType {
+    Normal, Private, ProxychainsNormal, ProxychainsPrivate, TMP
 };
 
-struct ListWidgetProfileData{
+struct ListWidgetProfileData {
     QString path;
     bool isDefault;
     ProfileWidgetType type;
     int priority;
-}; Q_DECLARE_METATYPE(ListWidgetProfileData)
+};
+Q_DECLARE_METATYPE(ListWidgetProfileData)
 
-class FirefoxProfileRunnerConfigForm : public QWidget, public Ui::FirefoxProfileRunnerConfigUi {
+class FirefoxProfileRunnerConfigForm: public QWidget, public Ui::FirefoxProfileRunnerConfigUi {
 Q_OBJECT
 
 public:
     explicit FirefoxProfileRunnerConfigForm(QWidget *parent);
 };
 
-class FirefoxRunnerConfig : public KCModule {
+class FirefoxRunnerConfig: public KCModule {
 Q_OBJECT
 
 public:
     explicit FirefoxRunnerConfig(QWidget *parent = nullptr, const QVariantList &args = QVariantList());
 
-    QIcon firefoxIcon, firefoxPrivateWindowIcon;
+    QIcon firefoxIcon;
+    const QIcon firefoxPrivateWindowIcon = QIcon::fromTheme("private_browsing_firefox");
+    const QIcon upIcon = QIcon::fromTheme("arrow-up");
+    const QIcon downIcon = QIcon::fromTheme("arrow-down");
     ProfileManager profileManager;
     QList<Profile> profiles;
     KSharedConfigPtr firefoxConfig;
