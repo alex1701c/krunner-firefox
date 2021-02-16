2b29a2f405cc00effba169cb63608691140b60a5
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2021-02-16 06:12:46 +0200

    Refactor install.sh and preinst scripts.

--- README.md
+++ README.md
@@ -13,80 +13,114 @@ You can find deb/rpm packages on the release page.
 
 ### Required Dependencies
 
-Debian/Ubuntu  
-`sudo apt install cmake extra-cmake-modules build-essential libkf5runner-dev libkf5textwidgets-dev qtdeclarative5-dev gettext libkf5kcmutils-dev`
+Debian/Ubuntu:
 
-openSUSE  
-`sudo zypper install cmake extra-cmake-modules libQt5Widgets5 libQt5Core5 libqt5-qtlocation-devel ki18n-devel ktextwidgets-devel kservice-devel krunner-devel gettext-tools kconfigwidgets-devel kcmutils-devel`  
+    sudo apt install cmake extra-cmake-modules build-essential libkf5runner-dev libkf5textwidgets-dev qtdeclarative5-dev gettext libkf5kcmutils-dev
 
-Fedora  
-`sudo dnf install cmake extra-cmake-modules kf5-ki18n-devel kf5-kservice-devel kf5-krunner-devel kf5-ktextwidgets-devel gettext kf5-kcmutils-devel`  
+openSUSE:
 
-Arch(Manjaro):  
-`sudo pacman -S cmake extra-cmake-modules kcmutils`  
+    sudo zypper install cmake extra-cmake-modules libQt5Widgets5 libQt5Core5 libqt5-qtlocation-devel ki18n-devel ktextwidgets-devel kservice-devel krunner-devel gettext-tools kconfigwidgets-devel kcmutils-devel  
 
-### Build instructions
+Fedora:
 
-The easiest way to install is:  
-`curl https://raw.githubusercontent.com/alex1701c/krunner-firefox/master/install.sh | bash`  
-Or you can install it without admin privileges:  
-`curl https://raw.githubusercontent.com/alex1701c/krunner-firefox/master/install-user.sh | bash`  
+    sudo dnf install cmake extra-cmake-modules kf5-ki18n-devel kf5-kservice-devel kf5-krunner-devel kf5-ktextwidgets-devel gettext kf5-kcmutils-devel  
 
-Or you can do it manually:  
-```
-git clone https://github.com/alex1701c/krunner-firefox
-cd krunner-firefox
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_QTPLUGINDIR=$(kf5-config --qt-plugins) ..
-make
-sudo make install
-kquitapp5 krunner 2> /dev/null; kstart5 --windowclass krunner krunner > /dev/null 2>&1 &
-```
+Arch/Manjaro:
+    sudo pacman -S cmake extra-cmake-modules kcmutils
 
-To get the icon for the private window you have to extract it:   
-```
-mkdir -p /tmp/firefoxprofile_installer
-unzip /usr/lib/firefox/browser/omni.ja -c "chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg" -d /tmp/firefoxprofile_installer > /dev/null 2>&1
-sudo mv /tmp/firefoxprofile_installer/chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg /usr/share/pixmaps/private_browsing_firefox.svg
-rm -rf /tmp/firefoxprofile_installer
-```
+### Quick Install
+
+Download the latest install script:
+
+    curl https://raw.githubusercontent.com/alex1701c/krunner-firefox/master/install.sh
+
+To install as a global plugin:
+
+    bash install.sh
+
+To install as a user plugin, admin privileges not needed:
+
+    GLOBAL_PLUGIN=false bash ./install.sh
+
+The install script can also be executed from the cloned repository:
+
+    git clone https://github.com/alex1701c/krunner-firefox
+    cd krunner-firefox
+    GLOBAL_PLUGIN=true bash ./install.sh
+
+### Manual build and install instructions
+
+To install as a global plugin:
+
+    git clone https://github.com/alex1701c/krunner-firefox
+    cd krunner-firefox
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_QTPLUGINDIR=$(kf5-config --qt-plugins) ..
+    make
+    sudo make -j$(nproc) install
+    kquitapp5 krunner 2> /dev/null; kstart5 --windowclass krunner krunner > /dev/null 2>&1 &
+
+To get the icon for the private window you have to extract it:
+
+    mkdir -p /tmp/firefoxprofile_installer
+    unzip /usr/lib/firefox/browser/omni.ja -c "chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg" -d /tmp/firefoxprofile_installer > /dev/null 2>&1
+    sudo mv /tmp/firefoxprofile_installer/chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg /usr/share/pixmaps/private_browsing_firefox.svg
+    rm -rf /tmp/firefoxprofile_installer
+
 If you install it manually for firefox-esr you should adjust the icon in src/plasma-runner-firefoxprofilerunner.desktop
-and change the path for the unzip command  
+and change the path for the unzip command.
+
 ## Screenshots
 
 #### Overview
-The plugin gets triggered by firef.  
+
+The plugin gets triggered by typing "firef" in KRunner:
+
 ![Overview](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/runner_profiles_overview.png)
 
 #### Search
-After the trigger word you can search the profiles.  
+
+After the trigger word you can search the profiles:
+
 ![Search](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/runner_profiles_search.png)
 
-#### Config Dialog  
-If you uncheck the options to register the normal/private window launch options profiles globally you can not launch
-the profiles like in the last screenshot.  
-If the following option is checked the option to launch a normal window with the default profile is not shown.
-This is useful, because the default profile can be launched with the "Applications" plugin.  
-The option to always show private windows is demonstrated in the first screenshot.  
+#### Config Dialog
 
-Using the Move Up/Down buttons you can change the order of the options.   
+If you uncheck the options to register the normal/private window launch options profiles globally you can not launch
+the profiles like in the last screenshot.
+
+If the following option is checked the option to launch a normal window with the default profile is not shown.
+This is useful, because the default profile can be launched with the "Applications" plugin.
+
+The option to always show private windows is demonstrated in the first screenshot.
+
+Using the Move Up/Down buttons you can change the order of the options.
+
 The text field below is used to rename the profiles. This does not modify the Firefox
-configuration, just the displayed name for the runner.  
-![Config Dialog](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/general_config_dialog.png)  
+configuration, just the displayed name for the runner.
+
+![Config Dialog](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/general_config_dialog.png)
 
 #### Config Dialog Proxychains
+
 If proxychains4 is installed you can add extra launch options. This way Firefox is started over a proxy
-but no system wide proxy has to be configured.  
+but no system wide proxy has to be configured.
+
 The force new instance button adds the "--new-instance" flag to the launch arguments of Firefox. This way
-you can be sure that Firefox is started over a proxy, but you can only have one instance of Firefox with 
-the selected profile running.  
-![Config Dialog Proxychains](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/config_proxychains_extra.png)  
-This configuration would look like this in Krunner:  
-![Proxychains Config Example](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/runner_profiles_search_proxychains.png)  
-The existing launch options can also be modified to use proxychains.  
-![Config Dialog Proxychains Change Existing](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/config_proxychains_existing.png)   
+you can be sure that Firefox is started over a proxy, but you can only have one instance of Firefox with
+the selected profile running.
 
-#### Overview With All Global Options    
+![Config Dialog Proxychains](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/config_proxychains_extra.png)
+
+This configuration would look like this in Krunner:
+
+![Proxychains Config Example](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/runner_profiles_search_proxychains.png)
+
+The existing launch options can also be modified to use proxychains.
+
+![Config Dialog Proxychains Change Existing](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/config_proxychains_existing.png)
+
+#### Overview With All Global Options
+
 ![Overview With All Global Options](https://raw.githubusercontent.com/alex1701c/Screenshots/master/FirefoxProfileRunner/global_overview_proxychains.png)
-
--- install.sh
+++ install.sh
@@ -1,36 +1,191 @@
 #!/bin/bash
+# Update bash options.
+# @see https://www.gnu.org/software/bash/manual/html_node/Modifying-Shell-Behavior.html
+# @see https://sipb.mit.edu/doc/safe-shell/
+# Exit script on error.
+set -o errexit
+# Error on unset (non-special) parameter expansion.
+set -o nounset
+# Disable pathname expansion.
+set -o noglob
 
-# Extract and move icon
-mkdir -p /tmp/firefoxprofile_installer
-if [[ -e /usr/lib/firefox/browser/omni.ja ]]; then
-    unzip /usr/lib/firefox/browser/omni.ja -c "chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg" -d /tmp/firefoxprofile_installer > /dev/null 2>&1
-else
-    unzip /usr/lib/firefox-esr/browser/omni.ja -c "chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg" -d /tmp/firefoxprofile_installer > /dev/null 2>&1
-    sed -i "s/Icon=.*/Icon=firefox-esr/" src/plasma-runner-firefoxprofilerunner.desktop
+readonly EXIT_CODE_GENERIC_ERROR=1
+
+function build_plugin() {
+    echo -e "\n\nBUILDING..."
+    if [[ -z "${SOURCE_DIR:-}" || ! -d "${SOURCE_DIR}" ]]; then
+        echo "No such directory: ${SOURCE_DIR}"
+        exit $EXIT_CODE_GENERIC_ERROR
+    fi
+    BUILD_DIR="${SOURCE_DIR}/build"
+    mkdir -p "${BUILD_DIR}"
+    cd "${BUILD_DIR}"
+    local CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release"
+    if [[ -z "${RUN_AS_ADMIN}" ]]; then
+        local USER_QT_PLUGINS="${HOME}/.local/lib/qt/plugins"
+        mkdir -p $USER_QT_PLUGINS
+        CMAKE_ARGS="${CMAKE_ARGS} -DKDE_INSTALL_QTPLUGINDIR=${USER_QT_PLUGINS}"
+        QT_PLUGIN_PATH="${QT_PLUGIN_PATH:-}"
+        if [[ -z "${QT_PLUGIN_PATH}" || "${QT_PLUGIN_PATH}" != *"/.local/lib/qt/plugins/"* ]]; then
+            # Add the installation path to the QT_PLUGIN_PATH
+            echo "export QT_PLUGIN_PATH=${USER_QT_PLUGINS}/:${QT_PLUGIN_PATH}" >> "${HOME}/.bashrc"
+            export QT_PLUGIN_PATH="${USER_QT_PLUGINS}/:${QT_PLUGIN_PATH}"
+        fi
+        local USER_SERVICES="${HOME}/.local/share/kservices5"
+        mkdir -p $USER_SERVICES
+        CMAKE_ARGS="${CMAKE_ARGS} -DKDE_INSTALL_KSERVICES5DIR=${USER_SERVICES}"
+
+    else
+        local USER_QT_PLUGINS=$(kf5-config --qt-plugins)
+        CMAKE_ARGS="${CMAKE_ARGS} -DKDE_INSTALL_QTPLUGINDIR=${USER_QT_PLUGINS}"
+    fi
+    cmake $CMAKE_ARGS ..
+
+    echo -e "\nBUILD FINISHED!"
+}
+
+function install_plugin() {
+    echo -e "\n\nINSTALLING..."
+    $RUN_AS_ADMIN make -j$(nproc) install
+
+    # Restart Krunner.
+    kquitapp5 krunner 2>/dev/null
+    kstart5 --windowclass krunner krunner >/dev/null 2>&1 &
+
+    echo -e "\nINSTALL FINISHED!"
+}
+
+function delete_temp_dir() {
+    if [[ -n "${TEMP_DIR:-}" && -d "${TEMP_DIR}" ]]; then
+        echo "Deleting temporary directory:"
+        echo " - ${TEMP_DIR}"
+        rm -rf "${TEMP_DIR}"
+    fi
+}
+
+function determine_firefox_install_path() {
+    FIREFOX_INSTALL_PATH="${FIREFOX_INSTALL_PATH:-}"
+    if [[ -z "${FIREFOX_INSTALL_PATH}" ]]; then
+        echo "Searching for firefox install path..."
+        local FIREFOX_INSTALL_DIR_CANDIDATES="
+            /usr/lib64/firefox
+            /usr/lib64/firefox-esr
+            /usr/lib/firefox
+            /usr/lib/firefox-esr
+        "
+        for CANDIDATE in ${FIREFOX_INSTALL_DIR_CANDIDATES}; do
+            if [[ -d "${CANDIDATE}" ]]; then
+                FIREFOX_INSTALL_PATH="${CANDIDATE}"
+                break;
+            fi
+        done
+        if [[ -z "${FIREFOX_INSTALL_PATH}" ]]; then
+            echo "Could not determine the firefox install directory. Please provide it using:"
+            echo "    export FIREFOX_INSTALL_PATH=/path/to/firefox"
+            exit $EXIT_CODE_GENERIC_ERROR
+        fi
+    fi
+    if [[ ! -d "${FIREFOX_INSTALL_PATH}" ]]; then
+        echo "No such directory: ${FIREFOX_INSTALL_PATH}"
+        exit $EXIT_CODE_GENERIC_ERROR
+    fi
+    if [[ ! -e "${FIREFOX_INSTALL_PATH}/firefox" ]]; then
+        echo "No firefox binary in: ${FIREFOX_INSTALL_PATH}"
+        exit $EXIT_CODE_GENERIC_ERROR
+    fi
+    echo "Found firefox in: ${FIREFOX_INSTALL_PATH}"
+}
+
+function handle_exit_signal() {
+    echo -e "\n\nCLEANUP..."
+    delete_temp_dir
+}
+
+function install_private_browsing_icon() {
+    echo "Installing private browsing icon:"
+    # The "omni.ja" archive contains an archive of various runtime files, including icons.
+    # @see https://developer.mozilla.org/en-US/docs/Mozilla/About_omni.ja_%28formerly_omni.jar%29
+    local OMNI_ARCHIVE_RELATIVE_PATH="browser/omni.ja"
+    local OMNI_ARCHIVE_FULL_PATH="${FIREFOX_INSTALL_PATH}/${OMNI_ARCHIVE_RELATIVE_PATH}"
+    echo " - extracting icon from: ${OMNI_ARCHIVE_FULL_PATH}"
+    if [[ ! -e "${OMNI_ARCHIVE_FULL_PATH}" ]]; then
+        echo "No such file: ${OMNI_ARCHIVE_FULL_PATH}"
+        exit $EXIT_CODE_GENERIC_ERROR
+    fi
+    local FAVICON_NAME="favicon.svg"
+    local FAVICON_PATH="chrome/browser/skin/classic/browser/privatebrowsing/${FAVICON_NAME}"
+    unzip -d "${TEMP_DIR}" -j -q "${OMNI_ARCHIVE_FULL_PATH}" "${FAVICON_PATH}"
+    if [[ "${FIREFOX_INSTALL_PATH}" == *firefox-esr ]]; then
+        # Change Icon name for ESR installations.
+        sed -i "s/Icon=.*/Icon=firefox-esr/" src/plasma-runner-firefoxprofilerunner.desktop
+    fi
+    # The icon name for private browsing profiles.
+    local NEW_ICON_NAME="private_browsing_firefox.svg"
+    # The icon path for private browsing profiles.
+    local NEW_ICON_DIR="/usr/share/pixmaps"
+    if [[ -z "${RUN_AS_ADMIN}" ]]; then
+        NEW_ICON_DIR="$HOME/.local/share/pixmaps"
+    fi
+    echo " - saving icon as: ${NEW_ICON_DIR}/${NEW_ICON_NAME}"
+    $RUN_AS_ADMIN mkdir -p "${NEW_ICON_DIR}"
+    $RUN_AS_ADMIN mv "${TEMP_DIR}/${FAVICON_NAME}" "${NEW_ICON_DIR}/${NEW_ICON_NAME}"
+}
+
+function prepare_temp_dir() {
+    local VENDOR_NAME="FirefoxProfileRunner"
+    TEMP_DIR="$(mktemp --directory --suffix="-${VENDOR_NAME}" --tmpdir=/tmp)"
+    echo "Using temporary directory:"
+    echo " - ${TEMP_DIR}"
+}
+
+function prepare_project_repo_dir() {
+    SOURCE_DIR="${PWD}"
+    # Clone project if it is downloaded using curl
+    if [[ ! -e "${SOURCE_DIR}/FirefoxProfileRunner.kdev4" ]]; then
+        echo "Cloning project repo..."
+        CMD_PATH="$(command -v git)"
+        if [[ -z "${CMD_PATH}" ]]; then
+            echo "Could not find git."
+            exit $EXIT_CODE_GENERIC_ERROR
+        fi
+        SOURCE_DIR="${TEMP_DIR}/repo"
+        git clone https://github.com/alex1701c/krunner-firefox "${SOURCE_DIR}"
+        cd "${SOURCE_DIR}"
+    fi
+}
+
+function prepare_build_and_install() {
+    echo "Preparing to build and install the plugin..."
+    if [[ "$(whoami)" == "root" ]]; then
+        echo "The project should not be built using root, installation will use sudo."
+        echo "Set 'GLOBAL_PLUGIN=false' for a user installation."
+        echo ""
+        echo "Installation canceled!"
+        exit 1
+    fi
+
+    # By default, the install script is executed with admin privileges.
+    RUN_AS_ADMIN="sudo"
+    if [[ "_${GLOBAL_PLUGIN:-}_" == "_false_" || "_${GLOBAL_PLUGIN:-}_" == "_FALSE_" ]]; then
+        RUN_AS_ADMIN=""
+    fi
+
+    # Register a cleanup function to be called on the EXIT signal. Regardless of the exit code.
+    trap handle_exit_signal EXIT
+
+    # Clone project repository if the script has been downloaded using curl/wget.
+    prepare_temp_dir
+    prepare_project_repo_dir
+
+    # Extract and install private browsing icon.
+    determine_firefox_install_path
+    install_private_browsing_icon
+}
+
+# Only run the install function when the script is executed and not when it's sourced.
+# @see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+if [[ ${FUNCNAME[0]:-} == "main" ]]; then
+    prepare_build_and_install
+    build_plugin
+    install_plugin
 fi
-
-# Exit immediately if something goes wrong
-set -e
-
-sudo mkdir -p /usr/share/pixmaps/
-sudo mv /tmp/firefoxprofile_installer/chrome/browser/skin/classic/browser/privatebrowsing/favicon.svg /usr/share/pixmaps/private_browsing_firefox.svg
-rm -rf /tmp/firefoxprofile_installer
-
-# Clone project if it is downloaded using curl
-if [[ $(basename "$PWD") != "krunner-firefox"* ]]; then
-  git clone https://github.com/alex1701c/krunner-firefox
-  cd krunner-firefox/
-fi
-
-mkdir -p build
-cd build
-
-cmake -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_QTPLUGINDIR=$(kf5-config --qt-plugins) ..
-make -j$(nproc)
-sudo make install
-
-# Restart Krunner
-kquitapp5 krunner 2>/dev/null
-kstart5 --windowclass krunner krunner >/dev/null 2>&1 &
-
-echo "Installation finished !"
